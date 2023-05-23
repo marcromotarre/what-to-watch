f120d154aa4dd70c4c9ec61d18d5fb55b8322d5b
Marc Romo <marc.romo@attackiq.com>
2023-05-23 15:50:29 +0200

    home

---
+++ src/components/pages/home.tsx
@@ -0,0 +1,31 @@
+import WidgetCarousel from "@/components/widgets/widget-carousel";
+import { userWidgetsState } from "@/states/user-state";
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+
+export default function HomePageComponent() {
+  const [userWidgets, setUserWidgets] = useRecoilState(userWidgetsState);
+  const [widgets, setWidgets] = useState([]);
+
+  useEffect(() => {
+    setWidgets(userWidgets)
+  }, [userWidgets]);
+
+  const widget_keys = Object.keys(widgets);
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#3D3D3D",
+        display: "grid",
+        gridTemplateColumns: "100%",
+        rowGap: 4,
+        paddingTop: 7,
+      }}
+    >
+      {widget_keys.map((widget_key: string) => (
+        <WidgetCarousel key={widget_key} {...userWidgets[widget_key].data} />
+      ))}
+    </Box>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,5 @@
-import user_widgets from "../data/widgets.json";
-import WidgetCarousel from "@/components/widgets/widget-carousel";
-import { userWidgetsState } from "@/states/user-state";
-import { Box } from "@mui/material";
-import { useRecoilValue } from "recoil";
+import HomePageComponent from "@/components/pages/home";
 
-export default function Home() {
-  const userWidgets = useRecoilValue(userWidgetsState)
-  const widget_keys = Object.keys(userWidgets)
-  return (
-    <Box
-      sx={{
-        backgroundColor: "#3D3D3D",
-        display: "grid",
-        gridTemplateColumns: "100%",
-        rowGap: 4,
-        paddingTop: 7,
-      }}
-    >
-      {widget_keys.map((widget_key: string) => (
-        <WidgetCarousel key={widget_key} {...userWidgets[widget_key].data} />
-      ))}
-    </Box>
-  );
+export default function HomePage() {
+  return <HomePageComponent />;
 }
